2212bd1d5af20dd1046108854c3148516e2d9028
Pablowolf96 <ikhena.owen47@gmail.com>
2022-07-10 18:03:54 +0100

    make Submit the surname Colombo in the HTML form

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -63,16 +63,16 @@ suite("Functional Tests", function () {
 });
 
 const Browser = require("zombie");
-const browser = new Browser();
-browser.site = "https://fcc-mochachai.pablowolf.repl.co";
+Browser.site = "https://fcc-mochachai.pablowolf.repl.co";
 
 suite("Functional Tests with Zombie.js", function () {
   this.timeout(5000);
+  const browser = new Browser();
   suiteSetup(function (done) {
     browser.visit("/", done);
   });
 
-  suite("Headless browser", function (done) {
+  suite("Headless browser", function () {
     test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
@@ -81,9 +81,15 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Colombo").then(() =>
+        browser.pressButton("submit", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+          done();
+        })
+      );
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
